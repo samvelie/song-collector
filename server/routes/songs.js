92916d1de6eb3b9d076182fbec86c6eb0ebf8748
Paige Bystrom <pdbystrom@gmail.com>
2017-04-06 14:06:48 -0500

    added basic song collection get

--- server/routes/songs.js
+++ server/routes/songs.js
@@ -4,6 +4,28 @@ var pg = require('pg');
 
 var pool = require('../modules/pg-pool');
 
+router.get('/', function(req, res) {
+  var userId = 1; // will become user id pulled from decoder token
+  pool.connect(function(err, client, done) {
+    if(err) {
+      console.log('error connecting to the database: ', err);
+      res.sendStatus(500);
+    } else {
+      client.query('SELECT * FROM song_collection WHERE user_id=$1;', [userId], function(err, result) {
+      if(err) {
+        console.log('error making database query: ', err);
+        res.sendStatus(500);
+      } else {
+        if(result.rows > 0) {
+          //
+        }
+        res.send(result.rows);
+        console.log('result.rows for songs get =', result.rows);
+      }
+      }); // end client.query
+    }
+  }); // end pool.connect
+}); //end router.get
 
 
 module.exports = router;
