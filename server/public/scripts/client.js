9e91960157e7b73858b87f1ed8fe0d1becd91fc2
Paige Bystrom <pdbystrom@gmail.com>
2017-04-07 16:23:18 -0500

    stylin

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,7 +12,7 @@ app.config(['$routeProvider', function($routeProvider) {
             controller: 'NewSongController',
             controllerAs: 'nsc'
         })
-        .when('/edit', {
+        .when('/edit/:songId?', {
             templateUrl: '/views/edit-song.html',
             controller: 'EditSongController',
             controllerAs: 'esc'
