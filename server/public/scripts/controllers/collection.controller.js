10da58e0047070e7dc2f6be79514b2b136fa4848
samvelie <sam.velie@gmail.com>
2017-04-12 10:26:23 -0500

    P.O.C. for show/hide on main view

--- server/public/scripts/controllers/collection.controller.js
+++ server/public/scripts/controllers/collection.controller.js
@@ -2,9 +2,16 @@ app.controller('CollectionController', ['SongFactory', function(SongFactory) {
   var self = this;
   console.log('in CollectionController');
   self.songs = SongFactory.songCollection;
-  self.showSong = SongFactory.showSong;
+  // self.showSong = SongFactory.showSong;
 
   var selectedSong = {};
+  self.songClicked = false;
+
+  self.showSong = function(index) {
+    self.songClicked = true;
+    console.log('songClicked:', self.songClicked);
+    console.log('show song of index ' + index);
+  }
 
   self.expandFilter = function() {
     if(self.spanClicked) {
