603d280b2bf7bb3f154dea35f205147c62f0ad33
Paige Bystrom <pdbystrom@gmail.com>
2017-04-13 14:27:17 -0500

    collection view changes

--- server/public/scripts/controllers/collection.controller.js
+++ server/public/scripts/controllers/collection.controller.js
@@ -11,7 +11,7 @@ app.controller('CollectionController', ['SongFactory', function(SongFactory) {
   self.fileUpload = SongFactory.fileUpload; //function for uploading
   self.filesUploaded = SongFactory.filesUploaded; //files for single song
   self.attachments = SongFactory.attachments; //attachments for single song
-
+  self.dropdowns = SongFactory.dropdowns;
 // have a promise so that after a song is deleted, user gets redirected back to the main card view
 // need to create a confirmation popup and an alert of deletion popup
   self.deleteFunction = function(songId) {
