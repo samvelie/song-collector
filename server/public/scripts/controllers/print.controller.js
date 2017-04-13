b1ca45762295c863714f9161b8cd235b85ca3335
Catherine J <cjarocki@gmail.com>
2017-04-13 12:31:24 -0500

    print view nanana

--- server/public/scripts/controllers/print.controller.js
+++ server/public/scripts/controllers/print.controller.js
@@ -1,7 +1,17 @@
-app.controller('PrintController', function() {
+app.controller('PrintController', ['SongFactory', '$routeParams', 'angularFilepicker', function (SongFactory, $routeParams, angularFilepicker) {
 
   var self = this;
 
+  console.log("print controller loaded yo");
+
   self.message = 'This is the PrintController View';
 
-});
+  self.songs = SongFactory.songCollection; // for the left-hand card view sidebar
+  self.fileUpload = SongFactory.fileUpload;
+  self.filesUploaded = SongFactory.filesUploaded; // will grab existing files uploaded by the user
+  // self.removeImage = SongFactory.removeImage;
+  self.oneSong = SongFactory.oneSong;
+  self.showSong = SongFactory.showSong;
+  self.attachments = SongFactory.attachments;
+
+}]);
