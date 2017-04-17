47101eb4f4d955f866fe9387348238ecd55194f7
Celina Peterson <celinap@gmail.com>
2017-04-17 14:33:47 -0500

    dropdown-multiselect displays a pre-selected setting

--- server/public/scripts/controllers/collection.controller.js
+++ server/public/scripts/controllers/collection.controller.js
@@ -15,7 +15,67 @@ app.controller('CollectionController', ['SongFactory', function(SongFactory) {
   self.filesUploaded = SongFactory.filesUploaded; //files for single song
   self.attachments = SongFactory.attachments; //attachments for single song
   self.dropdowns = SongFactory.dropdowns;
+
+  self.te_id = SongFactory.oneSong.details.teachable_elements_id;
+  self.te_value = SongFactory.oneSong.details.teachable_elements;
+  // self.testmodel = [{id: 1 }];
+  var taco = [
+    {
+      "id": 1,
+      "teachable_elements": "Two eigth note anacrusis"
+    }
+  ];
+
+  console.log('taco[0]', taco[0]);
+  console.log('self.testmodel is', self.testmodel);
+
+
+  // self.testmodel = [ taco[0] ];
+
+  // self.testing = true;
+	// self.testmodel = [{ id: 1 }, { id: 2 }];
+	// self.testdata = [
+	// 		{ id: 1, label: 'David' },
+	// 		{ id: 2, label: 'Jhon' },
+	// 		{ id: 3, label: 'Danny' },
+	// 	];
+	// self.testsettings = {
+	// 		selectionLimit: 1,
+	// 		selectedToTop: true,
+	// 		idProperty: 'id',
+  //     smartButtonMaxItems: 3,
+	// 	};
+
+
+  self.testing = true;
+  self.testmodel = [{ id: 1 }, { id: 3 }, { id: 5 }];
+  self.testdata = [
+      { id: 1, teachable_elements: 'eighth note anacrusis' },
+      { id: 2, teachable_elements: 'dotted quarter eighth' },
+      { id: 3, teachable_elements: 'Aeolian' },
+      { id: 4, teachable_elements: 'Asymmetrical meter' },
+      { id: 5, teachable_elements: 'Binary (AB)' },
+    ];
+  self.testsettings = {
+      displayProp: 'teachable_elements',
+      selectedToTop: true,
+      idProperty: 'id',
+      smartButtonMaxItems: 3,
+    };
+
+	// self.testevents = {
+	// 		onSelectionChanged() { // This event is not firing on selection of max limit
+	// 			$log.debug('you changed selection');
+	// 		},
+	// 	};
+
+
+  // self.editSongObject.teachableElementsModel = [
+  //   { 'id' : 1, 'teachable_elements' : 'Two eighth note anacrusis' },
+  // ];
+
   self.editSongObject.teachableElementsModel = [];
+
   self.teachableElementsSettings = {
     displayProp: 'teachable_elements',
     closeOnBlur: true,
@@ -24,7 +84,8 @@ app.controller('CollectionController', ['SongFactory', function(SongFactory) {
     enableSearch: true,
     smartButtonMaxItems: 10,
     scrollableHeight: '300px',
-    scrollable: true
+    scrollable: true,
+    selectedToTop: true
   };
 // have a promise so that after a song is deleted, user gets redirected back to the main card view
 // need to create a confirmation popup and an alert of deletion popup
