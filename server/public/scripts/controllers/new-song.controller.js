beb0001a013bd496421b953ca00f90622ff8cac8
samvelie <sam.velie@gmail.com>
2017-04-20 17:47:04 -0500

    new song clears fields and makes pristine after successful save

--- server/public/scripts/controllers/new-song.controller.js
+++ server/public/scripts/controllers/new-song.controller.js
@@ -30,9 +30,14 @@ app.controller('NewSongController', ['SongFactory','$location', '$scope', functi
       self.titlePlaceholder = "Title is required!";
       self.songError = true;
     } else {
-      SongFactory.saveNewSong(newSongObject);
-      console.log('saved!');
-      self.songError = false;
+      SongFactory.saveNewSong(newSongObject).then(function(){
+        console.log('saved!');
+        self.titlePlaceholder = "New Song Title";
+        self.songError = false;
+        self.newSongForm.$dirty = false;
+        self.newSongObject = {};
+      });
+
     }
   };
 
